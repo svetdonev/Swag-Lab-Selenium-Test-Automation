a3e6992803d169551e38aa90a64cdf599d4b0e2e
Svetlozar Donev <svetdonev1@gmail.com>
2024-08-28 20:29:10 +0300

    Create Login page class

---
+++ SwagLab Test Automation/LoginPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+
+namespace SwagLab_Test_Automation
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = "https://www.saucedemo.com/";
+        private IWebElement UsernameField => driver.FindElement(By.CssSelector("#user-name"));
+        private IWebElement PasswordField => driver.FindElement(By.CssSelector("#password"));
+        private IWebElement LoginButton => driver.FindElement(By.CssSelector("#login-button"));
+        private IWebElement ErrorMessage => driver.FindElement(By.CssSelector("[data-test='error']"));
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenWebsite() => driver.Navigate().GoToUrl(url);
+        public void EnterUsername(string username) => UsernameField.SendKeys(username);
+        public void EnterPassword(string password) => PasswordField.SendKeys(password);
+        public void ClearUsername() => UsernameField.Clear();
+        public void ClearPassword() => PasswordField.Clear();
+        public void ClickLoginButton() => LoginButton.Click();
+        public string GetErrorMessage() => ErrorMessage.Text;
+        public string GetPageTitle() => driver.Title;
+    }
+}
--- SwagLab Test Automation/obj/Debug/net8.0/SwagLab Test Automation.AssemblyInfo.cs
+++ SwagLab Test Automation/obj/Debug/net8.0/SwagLab Test Automation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwagLab Test Automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5c8044a85f32320523ab1c1450ebc4362587e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75322ff9cdd5f6e3e4322c925bf6f797aea0d06e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwagLab Test Automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwagLab Test Automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
