9b971ac40c08a178a637fa25e41a7882692244ca
Svetlozar Donev <svetdonev1@gmail.com>
2024-08-30 00:06:58 +0300

    Implement SpecFlow

--- SwagLab Test Automation/LoginSteps.cs
+++ SwagLab Test Automation/LoginSteps.cs
@@ -15,5 +15,61 @@ namespace SwagLab_Test_Automation
             driver = (IWebDriver)scenarioContext("WebDriver");
             loginPage = (LoginPage)scenarioContext("LoginPage");
         }
+
+        [Given(@"I am on the login page")]
+        public void GivenIamOnTheLoginPage()
+        {
+            loginPage.OpenWebsite();
+        }
+
+        [When(@"I attempt to login with empty credentials")]
+        public void WhenIAttemptToLoginWithEmptyCredentials()
+        {
+            loginPage.ClearUsername();
+            loginPage.ClearPassword();
+            loginPage.ClickLoginButton();
+        }
+
+        [When(@"I attempt to login with username and missing password")]
+        public void WhenIAttemptToLoginWithUsernameAndMissingPassword()
+        {
+            loginPage.EnterUsername("test-user");
+            loginPage.ClearPassword();
+            loginPage.ClickLoginButton();
+        }
+
+        [When(@"I attempt to login with locked out user")]
+        public void WhenIAttemptToLoginWithLockedOutUser()
+        {
+            loginPage.EnterUsername("locked_out_user");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.ClickLoginButton();
+        }
+
+        [When(@"I enter uesrname ""(.*)"" and password ""(.*)""")]
+        public void WhenIEnterUsernameAndPassword(string username, string password)
+        {
+            loginPage.EnterUsername("locked_out_user");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.ClickLoginButton();
+        }
+
+        [When(@"I click the login button")]
+        public void WhenIClickTheLoginButton()
+        {
+            loginPage.ClickLoginButton();
+        }
+
+        [Then(@"I should see an error message ""(.*)""")]
+        public void ThenIShouldSeeAnErrorMessage(string expectedMessage)
+        {
+            loginPage.GetErrorMessage().Should().Be(expectedMessage);
+        }
+
+        [Then(@"I should be redirected to the Swag Labs title page")]
+        public void ThenIShouldBeRedirectedToTheSwagLabsTitlePage()
+        {
+            loginPage.GetPageTitle().Should().Be("Swag Labs");
+        }
     }
 }
